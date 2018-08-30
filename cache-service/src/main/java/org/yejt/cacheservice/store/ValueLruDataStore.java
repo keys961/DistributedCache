94f7dc1f950cfefeb75d15c88a3a24ddf88e99ab
keys961 <keys961@hotmail.com>
2018-08-30 17:03:51 +0800

    Add Kryo byte value holder

--- cache-service/src/main/java/org/yejt/cacheservice/store/ValueLruDataStore.java
+++ cache-service/src/main/java/org/yejt/cacheservice/store/ValueLruDataStore.java
@@ -4,35 +4,39 @@ import org.yejt.cacheservice.store.value.ValueHolder;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ValueLruDataStore<K, V> implements DataStore<K, V>
 {
+    // max size, concurrency level (with avg size up to 2^n bytes)
+
     @Override
-    public ValueHolder<V> get(K key)
-    {
+    public ValueHolder<V> get(K key) {
         return null;
     }
 
     @Override
-    public ValueHolder<V> put(K key, V value)
-    {
+    public ValueHolder<V> put(K key, V value) {
         return null;
     }
 
     @Override
-    public ValueHolder<V> remove(K key)
-    {
+    public ValueHolder<V> remove(K key) {
         return null;
     }
 
     @Override
-    public Set<Map.Entry<K, ValueHolder<V>>> getAll()
-    {
+    public Set<Map.Entry<K, ValueHolder<V>>> getAll() {
         return null;
     }
 
     @Override
     public void clear() {
+
+    }
+
+    class Segment extends ReentrantLock
+    {
 
     }
 }
