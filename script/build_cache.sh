58edcda306f293021433d98c37cb70ed438f972a
keys961 <keys961@hotmail.com>
2018-12-26 14:14:31 +0800

    Update script

---
+++ script/build_cache.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Building cache-client..." && \
+cd ./cache-client && mvn install && cd .. && \
+echo "cache client build success." && \
+echo "Building cache-registry..." && \
+cd ./cache-registry && mvn package && cd .. && \
+echo "cache-registry build success." && \
+echo "Building cache-route..." && \
+cd ./cache-route && mvn package && cd .. && \
+echo "cache route build success." && \
+echo "Building cache-service..." && \
+cd ./cache-service && mvn package && cd .. && \
+echo "cache-service build success."
